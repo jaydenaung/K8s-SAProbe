cc457020a0a715118b0db59defff413a94ff35c6
Jayden Aung <jaydenaung@gmail.com>
2021-08-30 21:39:05 +0800

    initial script 1

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# K8s-Probes
-A collection of scripts that probe Kubernetes component
+# Kubernetes Probes
+
+Just a repo to share some useful scripts I've used in probing Kubernetes. I'll keep updating this repo as I come across new use cases.
+
+## Scripts 
+1. [autoprov_cfg_aws.sh](scripts/autoprov_cfg_aws.sh) - CME initialization script for AWS
+
---
+++ sa_secrets_probe.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+NAMESPACE=default
+pos=$(kubectl get pods | awk ' NR = 3 {print $1}' | sed 1d)
+PODWITHTOKEN=0
+echo "This script will scan if any service account mounted on any of the pods has access to any Kubernetes secrets.."
+for po in $pos
+do
+export sa=$(kubectl exec -it $po -- mount | grep serviceaccount | cut -d" " -f 3)
+  if [[ $sa != "/run/secrets/kubernetes.io/serviceaccount" ]]
+    then
+    echo "Service Account is not mounted on this Pod $po!"
+  else 
+    echo "POD $po has service account mounted here: $sa"
+    PODWITHTOKEN=$(( PODWITHTOKEN + 1 ))
+    echo "PROBING SERVICE ACCOUNT TOKEN.."
+    echo "POD $po has a SERVICE ACCOUNT TOKEN.."
+    TOKEN=$(kubectl exec -it $po -- cat /run/secrets/kubernetes.io/serviceaccount/token)
+    echo "$TOKEN"
+    echo "Probing to see whether this service account has access to any Kubernetes SECRETS in $NAMESPACE namespace.."
+    echo "HERE YOU GO.."
+    kubectl exec -it $po -- curl -k -H "Authorization: Bearer $TOKEN" \
+    -H 'Accept: application/json' \
+    https://kubernetes/api/v1/namespaces/$NAMESPACE/secrets/ 
+    sleep 1
+    echo "Probing to see whether this service account has access to any Kubernetes CONFIGMAPS in $NAMESPACE namespace.."
+    echo "HERE YOU GO.."
+    kubectl exec -it $po -- curl -k -H "Authorization: Bearer $TOKEN"  https://kubernetes/api/v1/namespaces/$NAMESPACE/configmaps/
+  fi
+sleep 1
+done
+echo "You have $PODWITHTOKEN Pods that have serviceaccount mounted."
+
+# -H 'Accept: application/json'
+
